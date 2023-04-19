74f0be16cec41be104cb065b81e06343f4daf809
zero2four <profistos@hotmail.com>
2023-04-19 22:21:34 +0200

    simulating many battles are now in an isolated method, battle result enum now return type of a single battle

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
 from ship import Ship, Fleet
+from enum import Enum
 
 
 
 
-# List of method names as strings
-attaker_list = [ 'dreadnauht', 'fighter']
-attaker_fleet = Fleet(attaker_list)
 
-defender_list = ['destroyer', 'destroyer']
-defender_fleet = Fleet(defender_list)
     
 
-print("attakers:", attaker_list)
-print("defenders:", defender_list)
+class BattleResult(Enum):
+    Attacker_win = 1
+    Defender_win = 2
+    Draw = 3
 
-
-def simulate_battle(defender_fleet, attaker_fleet):
+def simulate_battle(defender_fleet, attaker_fleet, make_battle_report = False) -> BattleResult:
     battle_round = 0
     while not defender_fleet.destroyed and not attaker_fleet.destroyed:
+        #ToDo - make an option to get more battle details 
 
         defender_hits = defender_fleet.produce_hits()
         attaker_hits = attaker_fleet.produce_hits()
@@ -26,40 +24,51 @@ def simulate_battle(defender_fleet, attaker_fleet):
         attaker_fleet.distribute_hits(defender_hits)
         
         if defender_fleet.destroyed and attaker_fleet.destroyed:
-            return "DRAW"
+            return BattleResult(3)
             
         if defender_fleet.destroyed:
-            return "ATTACKER WIN"
+            return BattleResult(1)
             
         if attaker_fleet.destroyed:
-            return "DEFENDER WIN"
+            return BattleResult(2)
 
-    
         battle_round += 1
         if battle_round > 100:
-            print("error")
+            raise RuntimeWarning("more then 100 iterations in a singe round of combat")
             break
 
-attacker_wins = 0
-defender_wins = 0
-draw = 0
 
-simulations = 10000
 
-for i in range(0,simulations,1):
+def simulate_many_battles(num_simulations:int, defender_fleet: Fleet, attaker_fleet:Fleet):
+    print("attakers:", attaker_list)
+    print("defenders:", defender_list)
+    attacker_wins = 0
+    defender_wins = 0
+    draw = 0
+    for i in range(0,num_simulations,1):
+        attaker_fleet = Fleet(attaker_list)
+        defender_fleet = Fleet(defender_list)
+        
+        result = simulate_battle(defender_fleet, attaker_fleet)
+        if result == BattleResult(1):
+            attacker_wins += 1
+        elif result == BattleResult(2):
+            defender_wins += 1
+        elif result == BattleResult(3):
+            draw += 1
+        
+    print("attacker wins: ", attacker_wins, f"({round((attacker_wins/num_simulations)*100,2)}%)")
+    print("defender wins: ", defender_wins,  f"({round((defender_wins/num_simulations)*100,2)}%)")
+    print("Draws: ", draw,  f"({round((draw/num_simulations)*100,2)}%)")
+
+
+if __name__ == "__main__":
+    # List of method names as strings
+    attaker_list = [ 'dreadnauht', 'fighter']
     attaker_fleet = Fleet(attaker_list)
+
+    defender_list = ['destroyer', 'destroyer']
     defender_fleet = Fleet(defender_list)
     
-    result = simulate_battle(defender_fleet, attaker_fleet)
-    if result == "ATTACKER WIN":
-        attacker_wins += 1
-    elif result == "DEFENDER WIN":
-        defender_wins += 1
-    elif result == "DRAW":
-        draw += 1
-        
-print("attacker wins: ", attacker_wins, f"({round((attacker_wins/simulations)*100,2)}%)")
-print("defender wins: ", defender_wins,  f"({round((defender_wins/simulations)*100,2)}%)")
-print("Draws: ", draw,  f"({round((draw/simulations)*100,2)}%)")
-
-
+    
+    simulate_many_battles(10000,defender_fleet, attaker_fleet )
