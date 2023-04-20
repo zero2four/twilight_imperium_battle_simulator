a659e9ed35d4ab1e8cded27dd6d2eb5dc7e72b8c
zero2four <profistos@hotmail.com>
2023-04-20 21:21:52 +0200

    config is now working

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def run():
-    subprocess.run(["streamlit", "run", ".\src\gui.py"], shell=True, check=True)
+    subprocess.run(["streamlit", "run", ".\src\gui\gui.py"], shell=True, check=True)
     
     
 if __name__ == "__main__":
